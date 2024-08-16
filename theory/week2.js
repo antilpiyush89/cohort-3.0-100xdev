e35048109fffd1a0516d8c41a27eeca4189b704c
antilpiyush89 <55829334+antilpiyush89@users.noreply.github.com>
2024-08-16 21:52:15 +0530

    Create week2.js

---
+++ theory/week2.js
@@ -0,0 +1,41 @@
+// const fs = require('fs');
+// const contents=fs.readFileSync('a.txt','utf-8');
+// console.log(contents);
+
+
+//Class in JS
+// class rectangle{
+//   constructor(width,height,color){
+//     this.width=width;
+//     this.height=height;
+//     this.color=color;
+
+//   }
+//   area() {
+//     const area= this.width*this.height;
+//     return area;
+//   }
+//   paint(){
+//     return "Color is: " + this.color;
+//   }
+// }
+// const rec= new rectangle(2,4,"Blue"); //instance or object
+// const rec1=new rectangle(1,2,"Red");
+// const area=rec.area();
+// const paint=rec.paint();
+// console.log(area);
+// console.log(paint);
+// console.log(rec1.area());
+// console.log(rec1.paint());
+
+
+
+// const map= new Map(); // created a object of map class
+// map.set('name','Piyush');
+// map.set('age',20);
+// console.log(map.get('name')); // I called a function on the object
+
+
+
+
+
