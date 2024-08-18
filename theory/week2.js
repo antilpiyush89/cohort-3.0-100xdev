6cddc2faf5c1c51c4a5fa8ee1e9ef16d39185f55
antilpiyush89 <55829334+antilpiyush89@users.noreply.github.com>
2024-08-18 10:26:45 +0530

    Update week2.js

--- theory/week2.js
+++ theory/week2.js
@@ -4,36 +4,36 @@
 
 
 //Class in JS
-// class rectangle{
-//   constructor(width,height,color){
-//     this.width=width;
-//     this.height=height;
-//     this.color=color;
+class rectangle{
+  constructor(width,height,color){
+    this.width=width;
+    this.height=height;
+    this.color=color;
 
-//   }
-//   area() {
-//     const area= this.width*this.height;
-//     return area;
-//   }
-//   paint(){
-//     return "Color is: " + this.color;
-//   }
-// }
-// const rec= new rectangle(2,4,"Blue"); //instance or object
-// const rec1=new rectangle(1,2,"Red");
-// const area=rec.area();
-// const paint=rec.paint();
-// console.log(area);
-// console.log(paint);
-// console.log(rec1.area());
-// console.log(rec1.paint());
+  }
+  area() {
+    const area= this.width*this.height;
+    return area;
+  }
+  paint(){
+    return "Color is: " + this.color;
+  }
+}
+const rec= new rectangle(2,4,"Blue"); //instance or object
+const rec1=new rectangle(1,2,"Red");
+const area=rec.area();
+const paint=rec.paint();
+console.log(area);
+console.log(paint);
+console.log(rec1.area());
+console.log(rec1.paint());
 
 
 
-// const map= new Map(); // created a object of map class
-// map.set('name','Piyush');
-// map.set('age',20);
-// console.log(map.get('name')); // I called a function on the object
+const map= new Map(); // created a object of map class
+map.set('name','Piyush');
+map.set('age',20);
+console.log(map.get('name')); // I called a function on the object
 
 
 
