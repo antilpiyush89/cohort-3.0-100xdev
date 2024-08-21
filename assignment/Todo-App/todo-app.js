448252cf4aee11542256521d9343707de98700df
antilpiyush89 <55829334+antilpiyush89@users.noreply.github.com>
2024-08-21 06:51:26 +0530

    Update todo-app.js

--- assignment/Todo-App/todo-app.js
+++ assignment/Todo-App/todo-app.js
@@ -1,3 +1,5 @@
+//Old code 
+
 let ctr = 1;
 function addtodo() {
   // Reading the data
@@ -15,4 +17,38 @@ function addtodo() {
 function deletetodo(ctr) {
   const element = document.getElementById(ctr);
   element.parentNode.removeChild(element);
+}
+
+
+
+// State derived Frontend code
+
+//State - Variable part of the app
+function addTodo(){
+  Todos=[]
+  Todos.push({
+    title:document.querySelector("input").value
+  })
+  render()
+}
+
+//Component- HTML and CSS part of the app(wireframe)
+function createTodoComponent(todo){
+  const div=document.createElement("div")
+  const h1=document.createElement("h1")
+  const button=document.createElement("button")
+  h1.innerHTML=todo.title
+  button.innerHTML="Delete"
+  div.appendChild(h1)
+  div.appendChild(button)
+  return div // Div ke ander h1(task info) and delete button pack krdia, createTodoComponent(todo)=div
+}
+
+//Render- printing the result on the website
+function render(){
+  document.querySelector("#todos").innerHTML=""
+  for(let i=0;i<todos.length;i++){
+    const element=createTodoComponent(Todos[i])
+    document.querySelector("#todos").appendChild(element)
+  }
 }
