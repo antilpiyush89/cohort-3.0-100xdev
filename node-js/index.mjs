49c5f9f31f8340739d1dde868a6a5462a3ce0568
antilpiyush89 <55829334+antilpiyush89@users.noreply.github.com>
2024-08-25 12:49:31 +0530

    Create index.mjs

---
+++ node-js/index.mjs
@@ -0,0 +1,5 @@
+import chalk from 'chalk'; //modern import syntax
+console.log(chalk.blue('Hello word'));
+console.log(chalk.red.bold('Hello word'));
+console.log(chalk.green.underline('Hello word'));
+
