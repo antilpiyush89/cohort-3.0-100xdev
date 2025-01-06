d18e95b2ab61833cbb150d677529be88c97ebe0e
antilpiyush89 <55829334+antilpiyush89@users.noreply.github.com>
2025-01-06 21:47:38 +0530

    client side rendering in nextjs

--- Week 19  Nextjs/testing/app/page.tsx
+++ Week 19  Nextjs/testing/app/page.tsx
@@ -1,7 +1,35 @@
+"use client"
+import axios from "axios"
+
+//useEffect,useState causes client side rendering -> html css being produced on client itself, waterfalling problem -> react does the same
+// To avoid client side rendering we should avoid using useEffect and useState
+import { useEffect,useState } from "react"
+
+
 export default function home(){
-  return(
-    <div>
-      Home
-    </div>
-  )
-}
+  const [data, setData] = useState<any>(null)
+  const [loader,setloader]=useState(true)
+
+  useEffect(()=>{
+    console.log("in useffect")
+    axios.get("https://jsonplaceholder.typicode.com/posts/1").then(response=>{
+      setData(response.data)
+      setloader(false)
+    })
+  },[])
+  if(loader){
+    console.log("hi")
+    return(
+      <div>
+        Loading....
+      </div>
+    )
+  }
+    return(
+      <div>
+        {data.userId}
+        {data.title}
+      </div>
+    )
+  }
+
