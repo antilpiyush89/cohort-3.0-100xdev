c2b78187d2464474e897d07cf3f3923a9009d67f
antilpiyush89 <55829334+antilpiyush89@users.noreply.github.com>
2025-01-09 21:36:33 +0530

    docs update

--- Flashgram-backend-testing/src/index.ts
+++ Flashgram-backend-testing/src/index.ts
@@ -1,5 +1,5 @@
 // save(bookmark), share, like
-
+// TASK -> comp harkirat videos, and discover more backend structures 
 
 
 
@@ -50,8 +50,6 @@ async function pdfloader(pdf:any){
 }
 
 async function llmAnswer(pdf:any){
-
-
   const genAI = new GoogleGenerativeAI("AIzaSyBxwi9eyjBKvqsasZAnCRgLSjpVCDtCFe4"); //api key given here
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash",systemInstruction: "You are a flashcard generator, I will give you a bunch of text, then you have to make flashcards out of it with question answer pairs, the flashcard may include a hint for answer and also return the whole data in a json, where it is like this -> flashcards:[{Question:{},Answer:{}},.....] and don't use any unecessary keywords like /n and ```json, bcz it will hinder the json.parse()" }); // system instructions
   const prompt = await pdfloader(pdf);
@@ -76,9 +74,6 @@ async function llmAnswer(pdf:any){
 //   console.log("flashcard inside the function: ",flashcards)
 //   return { flashcards };
 // }
-
-
-
 // console.log(JSON.stringify(formattedFlashcards, null, 2));
 
 app.listen(3000)
